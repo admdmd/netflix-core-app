619f3c484ef1e7bb03ce3e087462fc179f5ee17b
admdmd <ademideadeboye@gmail.com>
2024-10-13 13:05:19 +0100

    fix

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'netflix_core_app'
+    'netflix-site'
 ]
 
 MIDDLEWARE = [
