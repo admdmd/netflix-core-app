4067dc0e9ac07593bd265e264430c2dfeaafcbec
admdmd <ademideadeboye@gmail.com>
2024-10-17 01:12:20 +0100

    frontend

--- netflix_core_app/models.py
+++ netflix_core_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.conf import settings
 
 # Create your models here.
 class Movie(models.Model):
@@ -12,19 +13,25 @@ class Movie(models.Model):
         ('romance', 'Romance'),
         ('science_fiction', 'Science Fiction'),
         ('fantasy', 'Fantasy'),
-
     ]
 
     uu_id = models.UUIDField(default=uuid.uuid4)
-    title = models.CharField(max_lenght=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    release_date = models.Datefield()
-    genre = models.Charfield(max_lenght=100, choices=GENRE_CHOICES)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
     length = models.PositiveIntegerField()
     image_card = models.ImageField(upload_to='movie_images/')
     image_cover = models.ImageField(upload_to='movie_images/')
     video = models.FileField(upload_to='movie_videos/')
     movie_views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
+    
+class MovieList(models.Model):
+    owner_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'netflix_site',
+    'netflix_core_app',
     
 ]
 
